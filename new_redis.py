af2b2b142a740619dc1366c8f57277e997ac2284
hamed0731010 <109823124+hamed0731010@users.noreply.github.com>
2022-08-15 11:23:28 -0700

    Update new_redis.py

--- new_redis.py
+++ new_redis.py
@@ -1,17 +1,50 @@
 import redis
 from redis.commands.json.path import Path
 import json
-from  kucoin import  kucoin
 import threading
 import  requests
+from convert import  conv
 def new_redis():
     base_url = 'https://api.kucoin.com'
-    path = '/api/v1/market/histories'
-    threading.Timer(10.0, new_redis).start()
+    path = '/api/v1/market/allTickers'
+    threading.Timer(5.0, new_redis).start()
     u = requests.get(base_url + path, params={'symbol': 'XLM-USDT'})
     re = u.json()
     client1=redis.Redis(host='redis-19031.c232.us-east-1-2.ec2.cloud.redislabs.com',db=0, port=19031, password="4ENg0qBfOv9at2MUvzsMQ8mxXE3NZEvs")
     d = client1.json().set('person:1', Path.root_path(), re)
+
+def rate_list():
+    base_url = 'https://api.kucoin.com'
+    path = '/api/v1/market/allTickers'
+    threading.Timer(5.0, new_redis).start()
+    u = requests.get(base_url + path, params={'symbol': 'XLM-USDT'})
+    re = u.json()
+    client1 = redis.Redis(host='redis-19031.c232.us-east-1-2.ec2.cloud.redislabs.com', db=0, port=19031,
+                          password="4ENg0qBfOv9at2MUvzsMQ8mxXE3NZEvs")
+    #rem = client1.json().set('person:2', Path.root_path(), re)
+    jn = re['data']['ticker']
+    a=[]
+    b=[]
+
+    for x in jn:
+        a.append(x['symbol'])
+        b.append(x['sell'])
+
+    s=conv(a,b)
+
+    client1.set('foo', json.dumps(s))
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
