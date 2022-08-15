ad97ebc6e7bbd878513f7631143cace32171af88
hamed0731010 <109823124+hamed0731010@users.noreply.github.com>
2022-08-15 06:41:40 -0700

    Update new_redis.py

--- new_redis.py
+++ new_redis.py
@@ -7,7 +7,7 @@ import  requests
 def new_redis():
     base_url = 'https://api.kucoin.com'
     path = '/api/v1/market/histories'
-    threading.Timer(5.0, new_redis).start()
+    threading.Timer(10.0, new_redis).start()
     u = requests.get(base_url + path, params={'symbol': 'XLM-USDT'})
     re = u.json()
     client1=redis.Redis(host='redis-19031.c232.us-east-1-2.ec2.cloud.redislabs.com',db=0, port=19031, password="4ENg0qBfOv9at2MUvzsMQ8mxXE3NZEvs")
