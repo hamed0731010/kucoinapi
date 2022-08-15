9092ed56d11568ba8ea33c9d7710c18f4ed628cb
hamed0731010 <109823124+hamed0731010@users.noreply.github.com>
2022-08-15 11:24:23 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Union
-from new_redis import new_redis
+from new_redis import new_redis,rate_list
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.params import Depends
@@ -8,8 +8,8 @@ import redis
 import json
 
 app = FastAPI()
-data=new_redis()
-
+health_status=new_redis()
+list_rate=rate_list()
 
 class Item(BaseModel):
     name: str
@@ -18,13 +18,22 @@ class Item(BaseModel):
 
 
 @app.get("/")
-async def read_root():
-    data
+async def read_status():
+    health_status
     client1 = redis.Redis(host='redis-19031.c232.us-east-1-2.ec2.cloud.redislabs.com', db=0, port=19031,
                           password="4ENg0qBfOv9at2MUvzsMQ8mxXE3NZEvs")
-    d = client1.json().get('person:1')
-    return d
+    healthstatus = client1.json().get('person:1')
 
+    return healthstatus
+
+@app.get("/list/")
+async def read_list():
+    list_rate
+    client1 = redis.Redis(host='redis-19031.c232.us-east-1-2.ec2.cloud.redislabs.com', db=0, port=19031,
+                          password="4ENg0qBfOv9at2MUvzsMQ8mxXE3NZEvs")
+    ratelist = client1.get('foo')
+
+    return ratelist
 async def common_parameters(
     q: Union[str, None] = None, skip: int = 0, limit: int = 100):
     return {"q": q, "skip": skip, "limit": limit}
@@ -35,3 +44,4 @@ async def read_items(commons: dict = Depends(common_parameters)):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.price, "item_id": item_id}
+
